db7d99e4e565b239a6b4898677ff4a9ddcdf0dfc
toquete <toquete@rc.unesp.br>
2016-01-18 20:46:00 -0200

    constantes true e false

--- parser_only/teste.pas
+++ parser_only/teste.pas
@@ -18,6 +18,8 @@ var
 		    'teste': (d, e: string);
 		     5.68: (f, g: integer)
 	     end;
+  arr: array[0..100] of double;
+
 function proc (asd, asd: integer): boolean;
 begin
   for i := 0 downto c do
@@ -33,10 +35,37 @@ begin
 		xablau;
 	end;
 end;
+
 begin
-for ab:=0 to 5 do
-	guizao := 24;
-h := (not b) and c;
-teste := 'reasd';
+
+123:
+  a := b * (2 mod 5);
+  b := proc(1.40, 2);
+  proc2;
+
+  if a > 5.40e5 then
+  begin
+    repeat
+      a := a + b;
+    until false;
+  end
+  else
+  begin
+    while proc(84, 'teste') > 5 do
+      xablu('teste');
+  end;
+
+  case t of
+    5: with teste do
+       begin
+         goto 5;
+       end
+  end;
+  for ab:=0 to 5 do
+	  guizao := 24;
+
+  h := (not b) and c;
+
+  teste := 'reasd';
 end.
 
